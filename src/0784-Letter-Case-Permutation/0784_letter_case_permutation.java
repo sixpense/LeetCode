297e940bd570a99bda35fcce4a4dcd9d0178a874
sixpense <zhuoxiaosiat@gmail.com>
2020-08-28 23:54:15 +0800

    letter case permutation

---
+++ src/0784-Letter-Case-Permutation/0784_letter_case_permutation.java
@@ -0,0 +1,51 @@
+/*
+ * Letter Case Permutation
+ * Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.
+
+Return a list of all possible strings we could create. You can return the output  in any order.
+
+ 
+
+Example 1:
+
+Input: S = "a1b2"
+Output: ["a1b2","a1B2","A1b2","A1B2"]
+Example 2:
+
+Input: S = "3z4"
+Output: ["3z4","3Z4"]
+Example 3:
+
+Input: S = "12345"
+Output: ["12345"]
+Example 4:
+
+Input: S = "0"
+Output: ["0"]
+ */
+
+class Solution {
+    public List<String> letterCasePermutation(String S) {
+        List<StringBuilder> ans = new ArrayList();
+        ans.add(new StringBuilder());
+
+        for (char c: S.toCharArray()) {
+            int n = ans.size();
+            if (Character.isLetter(c)) {
+                for (int i = 0; i < n; ++i) {
+                    ans.add(new StringBuilder(ans.get(i)));
+                    ans.get(i).append(Character.toLowerCase(c));
+                    ans.get(n+i).append(Character.toUpperCase(c));
+                }
+            } else {
+                for (int i = 0; i < n; ++i)
+                    ans.get(i).append(c);
+            }
+        }
+
+        List<String> finalans = new ArrayList();
+        for (StringBuilder sb: ans)
+            finalans.add(sb.toString());
+        return finalans;
+    }
+}
